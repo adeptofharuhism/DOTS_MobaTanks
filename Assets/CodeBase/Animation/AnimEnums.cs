e56388c6dc3f5bb675ad1900ee8d0079c227a153
adeptofharuhism <nash.roma37@gmail.com>
2024-08-16 22:16:12 +0500

    Added Ranged mob model

--- Assets/CodeBase/Animation/AnimEnums.cs
+++ Assets/CodeBase/Animation/AnimEnums.cs
@@ -1,5 +1,7 @@
-public enum Model : short { MeleeMobAnimated }
-public static class ModelSize { public const short Size = 1; }
+public enum Model : short { MeleeMobAnimated, RangedMobAnimated }
+public static class ModelSize { public const short Size = 2; }
 
 public enum MeleeMobAnimated : byte { MeleeMob_Idle, MeleeMob_Attack, MeleeMob_Run, MeleeMob_Death }
 public static class MeleeMobAnimatedSize { public const byte Size = 4; }
+public enum RangedMobAnimated : byte { RangedMob_Idle, RangedMob_Attack, RangedMob_Run, RangedMob_Death }
+public static class RangedMobAnimatedSize { public const byte Size = 4; }
