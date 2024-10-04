e4c893beac6f11b1816ccd5532f977377953be14
adeptofharuhism <nash.roma37@gmail.com>
2024-10-04 16:55:31 +0500

    Going to save changes before abstract state machine realization

---
+++ Assets/CodeBase/UI/MainScene/MainSceneViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Zenject;
+
+namespace Assets.CodeBase.UI.MainScene
+{
+    public enum MainSceneViewMode
+    {
+        Loading,
+        Preparing,
+        InGame,
+        GameOver
+    }
+
+    public interface IMainSceneViewModel
+    {
+
+    }
+
+    public class MainSceneViewModel : IMainSceneViewModel, IInitializable, IDisposable
+    {
+        public MainSceneViewModel()
+        {
+            
+        }
+
+        public void Initialize() {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose() {
+            throw new NotImplementedException();
+        }
+    }
+}
